adf51cc5891695e5dfb5c69e21edae82c007a235
ifnavneetrajput <navneetrajput404@gmail.com>
2025-02-14 18:21:24 +0530

    Handle proptype error

--- src/Components/UserCard.jsx
+++ src/Components/UserCard.jsx
@@ -1,7 +1,8 @@
-
-const UserCard = ({user}) => {
-  const { firstName, lastName, age, gender, photoUrl ,about } = user;
+import PropTypes from "prop-types";
+const UserCard = ({ user }) => {
+  const { firstName, lastName, age, gender, photoUrl, about } = user;
   console.log(firstName)
+
   return (
     <div className="card bg-base-100 w-80 h-96  shadow-xl">
       <figure>
@@ -11,15 +12,27 @@ const UserCard = ({user}) => {
         />
       </figure>
       <div className="card-body">
-        <h2 className="card-title">{firstName +" " + lastName}</h2>
+        <h2 className="card-title">{firstName + " " + lastName}</h2>
+        {age &&<h3>{ age +","+ gender}</h3>}
+        
         <p>{about}</p>
         <div className="card-actions justify-end">
-          <button className="btn btn-primary">ignored</button>
-          <button className="btn btn-primary">interested</button>
+          <button className="btn btn-primary bg-red-400">ignored</button>
+          <button className="btn btn-primary bg-pink-800">interested</button>
         </div>
       </div>
     </div>
   );
 }
+UserCard.propTypes = {
+  user: PropTypes.shape({
+    firstName: PropTypes.string.isRequired, 
+    lastName: PropTypes.string.isRequired,
+    age: PropTypes.number, 
+    gender: PropTypes.string, 
+    photoUrl: PropTypes.string.isRequired, 
+    about: PropTypes.string, 
+  }).isRequired,
+};
 
 export default UserCard
